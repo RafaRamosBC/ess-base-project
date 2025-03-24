0fc95545949c9688372f8237bea7783f1a2d4b51
Pedro Gabriel <68481330+Pedro-Gabriel-Alves@users.noreply.github.com>
2025-03-24 04:52:52 -0300

    Update gerenciaUsuarios.js

--- backend/src/routes/gerenciaUsuarios.js
+++ backend/src/routes/gerenciaUsuarios.js
@@ -2,7 +2,7 @@ const express = require('express');
 const usuarios = express.Router();
 const { users } = require('../database/users_list.js');
 
-let nextUserId = 1;
+let nextUserId = users.length > 0 ? Math.max(...users.map(u => u.id)) + 1 : 1;
 
 // Função para encontrar um usuário pelo ID
 const findUserById = (id) => users.find(u => u.id === parseInt(id));
@@ -24,7 +24,7 @@ usuarios.get('/:id', (req, res) => {
 
 // Rota para criar um usuário
 usuarios.post('/', (req, res) => {
-  const { nome, login, senha } = req.body;
+  const { nome, login, senha, role } = req.body;
 
   // Validações
   if (!login){
@@ -45,7 +45,8 @@ usuarios.post('/', (req, res) => {
     id: nextUserId++,
     nome,
     login,
-    senha
+    senha,
+    role
   };
 
   users.push(newUser);
